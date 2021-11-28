ba195ebc9d31c492eb514c5448e98d149927cdc7
Mateus Henrique <mateusvillot@gmail.com>
2021-11-28 15:30:43 -0300

    Insert Spinner

--- Angular/src/app/courses/courses/courses.component.ts
+++ Angular/src/app/courses/courses/courses.component.ts
@@ -11,7 +11,7 @@ import { Course } from './../model/course';
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Observable <Course[]>;
+  courses$: Observable <Course[]>;
 
   displayedColumns = ['name' , 'category'];
 
@@ -19,7 +19,7 @@ export class CoursesComponent implements OnInit {
 
   constructor(private coursesService: CoursesService ) {
     //this.coursesService = new CoursesService();
-    this.courses = this.coursesService.list();
+    this.courses$ = this.coursesService.list();
 
   }
 
